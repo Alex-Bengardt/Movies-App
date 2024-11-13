64a22ec5b37da9850fa95b9193acc673bcfe096d
Alex-Bengardt <alexandragorky@yandex.ru>
2024-11-13 16:42:45 +0300

    first commit

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
-    return <h1>Hello world!</h1>;
+    return <h1>Hello world!</h1>
   }
 }
